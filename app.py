3e7ff6c088bb740ddea7559b47e6bef3e77fbf58
Lucas Levine <lucaslevine12@gmail.com>
2024-02-12 14:48:07 -0500

    main changed

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ async def main(page: ft.Page) -> None:
     await page.go_async("/login")
 
 
-app = flet_fastapi.app(main, assets_dir="/Users/lucaslevine/Documents/ISP 2/assets")
+# app = flet_fastapi.app(main, assets_dir="/Users/lucaslevine/Documents/ISP 2/assets")
+app = ft.app(main, assets_dir=r"/Users/lucaslevine/Documents/ISP 2/assets")
