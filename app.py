00768e9c77402ff50d571ae821cf9743db68ff50
Lucas Levine <lucaslevine12@gmail.com>
2024-02-12 14:29:28 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import flet as ft
+import flet_fastapi
+
+import views
+
+
+async def main(page: ft.Page) -> None:
+    page.title = "Pelicoin Banking"
+    page.fonts = {
+        "Kayak Light": "fonts/Kayak Sans Light.otf",
+        "Kayak Regular": "fonts/Kayak Sans Regular.otf",
+        "Kayak Bold": "fonts/Kayak Sans Bold.otf",
+    }
+
+    routeToView = {
+        "/login": views.getLogInView,
+        "/signup": views.getSignUpView,
+        "/accounts": views.getAccountsView,
+        "/accounts/current": views.getCurrentView,
+        "/accounts/education": views.getEducationView,
+        "/accounts/retirement": views.getRetirementView,
+    }
+
+    async def changeRoute(e: ft.RouteChangeEvent):
+        e.page.views.clear()
+        newView = await routeToView.get(e.route)(page)
+        e.page.views.append(newView)
+        await page.update_async()
+
+    page.on_route_change = changeRoute
+
+    await page.go_async("/login")
+
+
+app = flet_fastapi.app(main, assets_dir="/Users/lucaslevine/Documents/ISP 2/assets")
