49685a1321fa299b8a8838ee78dfe360d6eab96c
Lucas Levine <lucaslevine12@gmail.com>
2024-03-14 22:18:10 -0400

    idk anymore

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ async def main(page: ft.Page) -> None:
         global appView
         appView = ft.AppView.WEB_BROWSER
 
-        await micropip.install("psycopg2")
+        await micropip.install("psycopg")
 
     if local:
         page.window_width, page.window_height = 400, 850
